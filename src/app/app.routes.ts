a46be0d47a263b4fec0c0d994898ce154931f21e
BogdanMoskvitin <moskvitin.bg@gmail.com>
2025-12-13 17:56:00 +0300

    create header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Services } from './pages/services/services';
+import { Gallery } from './pages/gallery/gallery';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'services', component: Services },
+    { path: 'gallery', component: Gallery },
+    { path: '**', redirectTo: '' }
+];
